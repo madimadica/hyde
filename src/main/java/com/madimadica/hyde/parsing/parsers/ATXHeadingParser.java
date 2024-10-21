d704cedbeb26071f8159126edbbc3dc940eae856
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-10-20 20:29:37 -0500

    Improve ATXHeading Javadocs

--- src/main/java/com/madimadica/hyde/parsing/parsers/ATXHeadingParser.java
+++ src/main/java/com/madimadica/hyde/parsing/parsers/ATXHeadingParser.java
@@ -5,6 +5,30 @@ import com.madimadica.hyde.syntax.ATXHeading;
 
 import java.util.Optional;
 
+/**
+ * <a href="https://spec.commonmark.org/0.31.2/#atx-headings">CommonMark Spec - ATX headings</a>
+ * Parser for handling ATX Headings.
+ * <br>
+ * Defined as:
+ * <p>
+ * An ATX heading consists of a string of characters, parsed as inline content, between an opening sequence of 1–6 unescaped # characters and an optional closing sequence of any number of unescaped # characters. The opening sequence of # characters must be followed by spaces or tabs, or by the end of line. The optional closing sequence of #s must be preceded by spaces or tabs and may be followed by spaces or tabs only. The opening # character may be preceded by up to three spaces of indentation. The raw contents of the heading are stripped of leading and trailing space or tabs before being parsed as inline content. The heading level is equal to the number of # characters in the opening sequence.
+ * </p>
+ *
+ * <br>
+ * For example:
+ * <pre>
+ # foo
+ ## foo
+ ### foo
+ #### foo
+ ##### foo
+ ###### foo
+ * </pre>
+ *
+ * Which each translates into this HTML: <code>&lt;h1&gt;foo&lt;/h1&gt;</code> (for &lt;h1&gt; to &lt;h6&gt;)
+ *
+ * @see ATXHeading
+ */
 public class ATXHeadingParser implements Parser<ATXHeading> {
 
     public static final int MAX_HEADER_LEVEL = 6;
--- src/main/java/com/madimadica/hyde/syntax/ATXHeading.java
+++ src/main/java/com/madimadica/hyde/syntax/ATXHeading.java
@@ -3,25 +3,9 @@ package com.madimadica.hyde.syntax;
 import java.util.Objects;
 
 /**
- * <a href="https://spec.commonmark.org/0.31.2/#atx-headings">CommonMark Spec - ATX headings</a>
- * <br>
- * Defined as:
- * <p>
- * An ATX heading consists of a string of characters, parsed as inline content, between an opening sequence of 1–6 unescaped # characters and an optional closing sequence of any number of unescaped # characters. The opening sequence of # characters must be followed by spaces or tabs, or by the end of line. The optional closing sequence of #s must be preceded by spaces or tabs and may be followed by spaces or tabs only. The opening # character may be preceded by up to three spaces of indentation. The raw contents of the heading are stripped of leading and trailing space or tabs before being parsed as inline content. The heading level is equal to the number of # characters in the opening sequence.
- * </p>
+ * AST Node representing an <a href="https://spec.commonmark.org/0.31.2/#atx-headings">ATX Heading</a>.
  *
- * <br>
- * For example:
- * <pre>
- # foo
- ## foo
- ### foo
- #### foo
- ##### foo
- ###### foo
- * </pre>
- *
- * Which each translate into this HTML: <code>&lt;hr /&gt;</code>
+ * @see com.madimadica.hyde.parsing.parsers.ATXHeadingParser
  */
 public final class ATXHeading extends LeafBlock {
 
